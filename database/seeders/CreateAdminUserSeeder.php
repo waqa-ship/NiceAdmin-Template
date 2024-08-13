cff802a28b18675b63c67bb9d74a9478c057c9ba
waqas <waqas7211@gmail.com>
2024-08-14 04:27:13 +0500

    4commit

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -16,7 +16,7 @@ class CreateAdminUserSeeder extends Seeder
     public function run(): void
     {
         $user = User::create([
-            'name' => 'Hardik Savani', 
+            'name' => 'waqas', 
             'email' => 'admin@gmail.com',
             'password' => bcrypt('123456')
         ]);
--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -23,31 +23,46 @@
     </div>
 @endif
 
-<form method="POST" action="{{ route('roles.store') }}">
-    @csrf
+
+
+<div class="container mt-5">
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" placeholder="Name" class="form-control">
+        <div class="col-md-8">
+            <div class="card" style="background-color: #ffffff; border: 0.5px solid #dee2e6;">
+                <div class="card-body">
+                   
+                    <div class="card-text"><form method="POST" action="{{ route('roles.store') }}">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-4 mt-3">
+                                <div class="form-group">
+                                    <strong>Name:</strong>
+                                    <input type="text" name="name" placeholder="Name" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Permission:</strong>
+                                    <br/>
+                                    @foreach($permission as $value)
+                                        <label><input type="checkbox" name="permission[{{$value->id}}]" value="{{$value->id}}" class="name">
+                                        {{ $value->name }}</label>
+                                    <br/>
+                                    @endforeach
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-sm mt-3 float-start">
+                                    <i class="fa-solid fa-floppy-disk"></i> Submit
+                                </button>
+                            </div>
+                        </div>
+                    </form></div>
+                
+                </div>
             </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Permission:</strong>
-                <br/>
-                @foreach($permission as $value)
-                    <label><input type="checkbox" name="permission[{{$value->id}}]" value="{{$value->id}}" class="name">
-                    {{ $value->name }}</label>
-                <br/>
-                @endforeach
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary btn-sm mb-3"><i class="fa-solid fa-floppy-disk"></i> Submit</button>
         </div>
     </div>
-</form>
-
+</div>
 
 @endsection
